e05d464b017a1dfcf2be52300a958e12f95289cf
Andy von Dohren <avondohren@gmail.com>
2014-04-02 15:18:38 -0500

    initial build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTictactoe::Application.config.secret_token = '823d31d40529c5ba41b8a63f1c1cd1005917894fe8acc39d3bf0f665eccad808060af5745f726933d572e837f04d6f9417db16a0ff2606f531461d58fd371208'
