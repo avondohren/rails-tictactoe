f4876b7fe8871abf0da2ba571439fb8c7ea6c460
Andy von Dohren <avondohren@gmail.com>
2014-04-02 23:11:04 -0500

    working

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -9,7 +9,6 @@ $(document).ready (function(){
     var currentPlayer = "X";
     
     refreshScreen();
-    console.log("Board: " + boardArray);
   
     function swapPlayer (){
       if (currentPlayer === "X"){
@@ -26,15 +25,69 @@ $(document).ready (function(){
       if (boardArray[myIndex] === ""){
         $(element).text(currentPlayer);
         boardArray[myIndex] = currentPlayer;
-        swapPlayer();
+        $("#game_board").first().val(JSON.stringify( boardArray ));
+        if (winner(boardArray) !== true){
+          swapPlayer();
+          if (currentPlayer === "O"){
+            comp_play();
+          }
+        }
       }
     }
     
+    function comp_play(){
+      var comp_position;
+      // Try to win by adding a computer position
+      comp_position = simulate("O");
+      
+      // Try to block by detecting where a player might play to win
+      if (comp_position === undefined){
+        comp_position = simulate("X");
+      }
+      
+      if (comp_position === undefined){
+        comp_position = random();
+      }
+      
+      markSquare($(".square:nth-of-type(" + (comp_position + 1) + ")"));
+    }
+    
+    function random(){
+      for(var i=0; i<=8; i++){
+        if (isOpen(i) === true){
+          return i;
+        }
+      }
+    }
+    
+    function simulate(player){
+      var tmp_array = boardArray.slice(0);
+      var position;
+      
+      for(var i=0; i<=8; i++){
+        if ( isOpen(i) === true ){
+          tmp_array[i] = player;
+          if (winner(tmp_array) === true){
+            position = i;
+            break;
+          } else {
+            tmp_array[i] = "";
+          }
+        }
+      }
+      return position;
+    }
+    
+    function isOpen(position){
+      if (boardArray[position] === ""){
+        return true;
+      }
+      return false;
+    }
+    
+    
     function refreshScreen (){
       $(".square").each(function () {
-        console.log("board: " + boardArray);
-        console.log($(this).index());
-        console.log(boardArray[$(this).index()])
         if (boardArray[$(this).index()] === "X"){
           $(this).text("X");
         } else if (boardArray[$(this).index()] === "O"){
@@ -43,27 +96,26 @@ $(document).ready (function(){
           $(this).text("")
         }
       });
-      console.log("board: " + boardArray)
     }
     
-    function winner() {
+    function winner(myArray) {
       var winner = false;
     
-      if (boardArray[0] !== "" && boardArray[0] === boardArray[1] && boardArray[1] === boardArray[2]){
+      if (myArray[0] !== "" && myArray[0] === myArray[1] && myArray[1] === myArray[2]){
         winner = true;
-      } else if (boardArray[3] !== "" && boardArray[3] === boardArray[4] && boardArray[4] === boardArray[5]) {
+      } else if (myArray[3] !== "" && myArray[3] === myArray[4] && myArray[4] === myArray[5]) {
         winner = true;
-      } else if (boardArray[6] !== "" && boardArray[6] === boardArray[7] && boardArray[7] === boardArray[8]) {
+      } else if (myArray[6] !== "" && myArray[6] === myArray[7] && myArray[7] === myArray[8]) {
         winner = true;
-      } else if (boardArray[0] !== "" && boardArray[0] === boardArray[3] && boardArray[3] === boardArray[6]) {
+      } else if (myArray[0] !== "" && myArray[0] === myArray[3] && myArray[3] === myArray[6]) {
         winner = true;
-      } else if (boardArray[1] !== "" && boardArray[1] === boardArray[4] && boardArray[4] === boardArray[7]) {
+      } else if (myArray[1] !== "" && myArray[1] === myArray[4] && myArray[4] === myArray[7]) {
         winner = true;
-      } else if (boardArray[2] !== "" && boardArray[2] === boardArray[5] && boardArray[5] === boardArray[8]) {
+      } else if (myArray[2] !== "" && myArray[2] === myArray[5] && myArray[5] === myArray[8]) {
         winner = true;
-      } else if (boardArray[0] !== "" && boardArray[0] === boardArray[4] && boardArray[4] === boardArray[8]) {
+      } else if (myArray[0] !== "" && myArray[0] === myArray[4] && myArray[4] === myArray[8]) {
         winner = true;
-      } else if (boardArray[2] !== "" && boardArray[2] === boardArray[4] && boardArray[4] === boardArray[6]) {
+      } else if (myArray[2] !== "" && myArray[2] === myArray[4] && myArray[4] === myArray[6]) {
         winner = true;
       }
     
@@ -74,8 +126,7 @@ $(document).ready (function(){
       if ($("#game_won").first().prop('checked') !== true){
         markSquare(this);
     
-        if (winner() === true) {
-          swapPlayer();
+        if (winner(boardArray) === true) {
           $("#player").first().text(currentPlayer + " WINS!");
           $("#game_board").first().val(JSON.stringify( boardArray ));
           $("#game_won").first().prop('checked', true);
