740fe075a687f752e1d3cb1a4b8751a92f8beb07
Andy von Dohren <avondohren@gmail.com>
2014-04-04 13:58:47 -0500

    generate models and controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,25 +1,29 @@
 class GamesController < ApplicationController
   def index
+    authorize
     @games = current_user.games.all
   end
 
   def show
+    authorize
     @game = Game.find(params[:id])
   end
 
   def create
-    board = Array.new(9)
-    game = Game.create({:user_id => session[:user_id], :board => @board})
+    authorize
+    game = Game.create({:user_id => session[:user_id]})
     redirect_to(game_path(game.id))
   end
 
   def update
+    authorize
     game = Game.find(params[:id])
     game.update_attributes(params[:game])
     redirect_to(games_path)
   end
 
   def destroy
+    authorize
     game = Game.find(params[:id])
     game.delete
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 RailsTictactoe::Application.routes.draw do
+  get "transaction/index"
+
+  get "transaction/show"
+
+  get "transaction/new"
+
+  get "transaction/create"
+
+  get "transaction/edit"
+
+  get "transaction/update"
+
+  get "transaction/destroy"
+
+  get "account/index"
+
+  get "account/show"
+
+  get "account/new"
+
+  get "account/create"
+
+  get "account/edit"
+
+  get "account/update"
+
+  get "account/destroy"
+
+  get "user/index"
+
+  get "user/show"
+
+  get "user/new"
+
+  get "user/create"
+
+  get "user/edit"
+
+  get "user/update"
+
+  get "user/destroy"
+
   root to: 'logins#new'
   resources :users
   resources :games, :except => [:new, :edit]
